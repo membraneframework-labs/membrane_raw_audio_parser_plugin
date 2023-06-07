acd897e22aaee4a86cd8c31468ba4b002ced517e
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-06-07 15:56:45 +0200

    bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RawAudioParser.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane_raw_audio_parser_plugin"
 
   def project do
@@ -36,7 +36,7 @@ defmodule Membrane.RawAudioParser.Mixfile do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.11.0"},
+      {:membrane_core, "~> 0.12.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
