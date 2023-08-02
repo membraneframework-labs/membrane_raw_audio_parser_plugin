f31523ccb8192328f846b1dbeba38fac21422bc9
Killian McGuinness <killian@naillik.ca>
2023-08-02 11:23:18 -0400

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RawAudioParser.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @github_url "https://github.com/membraneframework/membrane_raw_audio_parser_plugin"
 
   def project do
